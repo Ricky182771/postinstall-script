1a7b9a6bef4420caaff37cc831936320aa70f3d8
Rickwiner <119073241+Ricky182771@users.noreply.github.com>
2026-05-22 15:32:15 -0600

    Remove echo statements and streamline installation

--- rpmfusiononly.sh
+++ rpmfusiononly.sh
@@ -2,19 +2,8 @@
 
 FEDORA_VERSION=$(rpm -E %fedora)
 
-echo "Instalando RPM Fusion para Fedora $FEDORA_VERSION..."
 
 wget https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$FEDORA_VERSION.noarch.rpm
 wget https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm
 
 sudo dnf install -y ./rpmfusion-free-release-$FEDORA_VERSION.noarch.rpm ./rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm
-
-echo "¡RPM Fusion instalado!"
-
-echo "Descargando RustDesk..."
-
-wget https://github.com/rustdesk/rustdesk/releases/download/1.4.6/rustdesk-1.4.6-0.x86_64.rpm
-
-sudo dnf install -y ./rustdesk-1.4.6-0.x86_64.rpm
-
-echo "¡Listo! RPM Fusion y RustDesk instalados correctamente."
